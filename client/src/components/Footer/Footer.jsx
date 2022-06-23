bfa9ff0c765ed65c85070c7045b49aea3b695016
lexCarey <lexcarey73@gmail.com>
2022-06-22 19:38:56 -0700

    Footer Done

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -5,7 +5,11 @@ const Footer = () => {
     return (
         <footer>
             <div>
-                
+                Thanks for checking out my portfolio site!
+            </div>
+            <div className='right-footer'>
+                <p className='email'>Lexington Carey</p>
+                <p>LexCarey73@gmail.com</p>
             </div>
         </footer>
     )
