7c55008b565d41a3cb81427c95b5d7e737a0592b
lexCarey <lexcarey73@gmail.com>
2022-06-30 18:34:17 -0700

    Better Email Form using Material UI

--- client/src/components/EmailForm/EmailForm.jsx
+++ client/src/components/EmailForm/EmailForm.jsx
@@ -1,6 +1,8 @@
 import './EmailForm.css'
 import React, { useRef } from 'react';
 import emailjs from '@emailjs/browser';
+import { TextField, TextareaAutosize } from '@mui/material';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 export const EmailForm = () => {
     const form = useRef();
@@ -17,21 +19,74 @@ export const EmailForm = () => {
     form.current.reset()
     };
 
+    const theme = createTheme({
+        palette: {
+            primary: {
+                main: '#8f9cca'
+            },
+        },
+    });
+
+    const styles = {
+        floatingLabelFocusStyle: {
+            color: "#8f9cca"
+        }
+    }
+
     return (
     <div className='email-form'>
         <form ref={form} onSubmit={sendEmail}>
-            <input className='w100' type="text" name="subject" placeholder='Subject' />
-            <br />
+            <ThemeProvider theme={theme}>
+                <TextField
+                className='text-field'
+                fullWidth
+                id="filled-basic"
+                label="Subject"
+                variant="filled"
+                name='subject'/>
             <div className='flex'>
-                <input className='w50 marg-r-10' type="text" name="name" placeholder='Name' />
-                <input className='w50' type="email" name="email" placeholder='Email' />
+                <TextField
+                className='text-field marg-r-15'
+                fullWidth
+                id="filled-basic"
+                label="Name"
+                variant="filled"
+                name='name'/>
+                <TextField
+                className='text-field'
+                fullWidth
+                id="filled-basic"
+                label="Email"
+                variant="filled"
+                name='email'
+                type="email"/>
             </div>
-            <textarea className='w100' name="message" placeholder='Message' rows="6" />
+            <TextField
+            className='text-field'
+            fullWidth
+            id="filled-basic"
+            label="Message"
+            variant="filled"
+            name='message'
+            multiline
+            minRows={4}
+            maxRows={8}/>
             <br />
-            <input type="submit" value="Send" />
+            <input type="submit" value="Send"/>
+            </ThemeProvider>
         </form>
     </div>
     );
 };
 
 export default EmailForm
+
+/* <input className='w100' type="text" name="subject" placeholder='Subject' />
+<br />
+<div className='flex'>
+    <input className='w50 marg-r-10' type="text" name="name" placeholder='Name' />
+    <input className='w50' type="email" name="email" placeholder='Email' />
+</div>
+<textarea className='w100' name="message" placeholder='Message' rows="6" />
+<br />
+<input type="submit" value="Send" /> */
